847c5a63428fe27969aa3c4bab209ba2500a69d1
superyngo <superyngo@gmail.com>
2023-09-21 11:57:26 +0800

    new_UI

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config'
-import netlify from '@astrojs/netlify/functions'
+// import netlify from '@astrojs/netlify/functions'
 
 import svelte from '@astrojs/svelte'
 
