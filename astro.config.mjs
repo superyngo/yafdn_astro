53cfb4098f5f9b19fe6df2e05c2a4945c3c48c4d
superyngo <superyngo@gmail.com>
2023-09-21 11:34:24 +0800

    UPDATE

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import netlify from '@astrojs/netlify/functions'
 
-import svelte from "@astrojs/svelte";
+import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
-});
+  integrations: [svelte()],
+  output: 'server',
+  adapter: netlify()
+})
