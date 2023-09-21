170f07a216b89cc18669dd86956dda4fe27517b8
superyngo <superyngo@gmail.com>
2023-09-21 11:45:34 +0800

    new_UI

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
-  output: 'server',
-  adapter: netlify()
+  integrations: [svelte()]
+  // output: 'server',
+  // adapter: netlify()
 })
