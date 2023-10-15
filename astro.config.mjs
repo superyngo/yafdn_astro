6d9ce2e7b66ee05e7a3fbcfd276e0d9e3b8311a3
superyngo <superyngo@gmail.com>
2023-10-15 22:03:55 +0800

    fix_bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config'
-import remarkToc from 'remark-toc'
+// import remarkToc from 'remark-toc'
+import path from 'path'
 // import netlify from '@astrojs/netlify/functions'
+import * as url from 'url'
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
 import svelte from '@astrojs/svelte'
 
@@ -8,6 +11,13 @@ import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    }
+  },
   markdown: {
     // Applied to .md and .mdx files
     // remarkPlugins: [[remarkToc, { ordered: true, tight: false }]]
